3ed647481ab8f99659c0f73584ab2e48b3dddef8
M Fachri <mfachri@upi.edu>
2021-05-16 18:32:19 +0700

    Add files via upload

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -3,6 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Providers\RouteServiceProvider;
+use App\Models\Worker;
+use App\Models\User;
+use App\Models\Service;
+use App\Models\Order;
 
 class OrdersController extends Controller
 {
@@ -13,7 +20,27 @@ class OrdersController extends Controller
      */
     public function index()
     {
-        //
+        return view('pesan.ringkasan');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    // public function bayar()
+    // {
+    //     return view('pesan.bayar');
+    // }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function done()
+    {
+        return view('pesan.selesai');
     }
 
     /**
@@ -23,7 +50,7 @@ class OrdersController extends Controller
      */
     public function create()
     {
-        //
+
     }
 
     /**
@@ -33,6 +60,29 @@ class OrdersController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
+    {
+        $workId = Service::where('id',$request->service_id)->first();
+
+        $order = Order::create([
+            'order_number' => 'ORD-'.strtoupper(uniqid()),
+            'status' => 'menunggu',
+            'worker_id' => $workId->workers_id,
+            'customer_id' => Auth::user()->id,
+            'service_id' => $request->service_id
+        ]);
+    
+        $order->save();
+
+        return redirect('order/bayar');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function bayarStore(Request $request)
     {
         //
     }
